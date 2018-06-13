e810675b97c5e88d5a350e31ea32f497372f5306
tianyahechy <tianyahechu2004@163.com>
2018-06-13 16:49:48 +0800

    拆开每个变量和内存对象的函数

--- main.cpp
+++ main.cpp
@@ -2,14 +2,22 @@
 #include "myOpenCL.h"
 #include <vector>
 
+//读写内存
+enum readWrite
+{
+	readOnly = 0,
+	readWrite = 1
+};
+
 int main(int argc, char ** argv)
 {
 	std::string strFileName = "test.cl";
 	std::string strOpenCLKernalEntry = "hello_kernel";
-	myOpenCL theOpenCL(strFileName, strOpenCLKernalEntry);
-
 	int objectSize = 3;
 	int numberOfEachObject = ARRAY_SIZE;
+	//读写vector,用以判断是否
+	myOpenCL theOpenCL(strFileName, strOpenCLKernalEntry);
+
 	std::vector<cl_mem> memObjectVector;
 	memObjectVector.clear();
 	memObjectVector.resize(objectSize);
@@ -30,8 +38,11 @@ int main(int argc, char ** argv)
 		computeVector[0][i] = (float)i;
 		computeVector[1][i] = (float)(i * 2);
 	}
-	theOpenCL.createMemObjects(&memObjectVector[0], &computeVector[0][0], &computeVector[1][0]);
-
+	//theOpenCL.createMemObjects(&memObjectVector[0], &computeVector[0][0], &computeVector[1][0]);
+	cl_context theContext = theOpenCL.getContext();
+	memObjectVector[0] = clCreateBuffer(theContext, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * ARRAY_SIZE, &computeVector[0][0], NULL);
+	memObjectVector[1] = clCreateBuffer(theContext, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * ARRAY_SIZE, &computeVector[1][0], NULL);
+	memObjectVector[2] = clCreateBuffer(theContext, CL_MEM_READ_WRITE, sizeof(float) * ARRAY_SIZE, NULL, NULL);
 	//建立内核参数
 	for (size_t i = 0; i < objectSize; i++)
 	{
